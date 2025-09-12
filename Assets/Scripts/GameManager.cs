f12977ae751a286551e05b47d2eefe51591a7cea
KayTen24 <kay-ten@hotmail.com>
2025-09-12 14:32:36 -0400

    Removed Coins and Flag. Added Timer.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,21 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public int score;
-    public TMP_Text scoreText; 
+    public TMP_Text scoreText;
+    public float Timer = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "Score: " + score;
+        Timer -= Time.deltaTime;
+        if (Timer <= 0)
+        {
+            Debug.Log("Game Ended");
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(2);
+            //End the Game
+        }
+        
+        {
+            scoreText.text = "Score: " + score;
+        }
     }
+
 }
+
